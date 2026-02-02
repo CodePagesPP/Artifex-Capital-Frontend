ea719d883f9a86fb2fb6918d85ff7e942f5cc865
Espinozaje <jeisson12aaron@gmail.com>
2026-02-02 18:24:02 -0500

    landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./landing/landing.component').then(m => m.LandingComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
