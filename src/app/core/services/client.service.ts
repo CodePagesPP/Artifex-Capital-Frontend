1cff9445bb0739288bddb9c2cbec686a8aedbfba
Espinozaje <jeisson12aaron@gmail.com>
2026-04-14 23:46:57 -0500

    environment

--- src/app/core/services/client.service.ts
+++ src/app/core/services/client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { Client, ClientUpdateData, ContactForm, Page } from '../models/client.model';
