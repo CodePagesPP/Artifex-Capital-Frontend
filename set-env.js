591acf482355b49dda5fcba3584cc89cc81cac53
Espinozaje <jeisson12aaron@gmail.com>
2026-04-14 23:52:46 -0500

    deploy v5

--- set-env.js
+++ set-env.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 
 
+const targetPathProd = './src/environments/environment.prod.ts';
 const targetPathDev = './src/environments/environment.ts'; 
 
+
 const backendUrl = process.env.URL_BACKEND || 'http://localhost:8080/api/v1';
+
+
+const envConfigFileProd = `
+export const environment = {
+  production: true,
+  apiUrl: '${backendUrl}'
+};
+`;
+
 
 const envConfigFileDev = `
 export const environment = {
@@ -12,6 +23,8 @@ export const environment = {
 };
 `;
 
+
+fs.writeFileSync(targetPathProd, envConfigFileProd);
 fs.writeFileSync(targetPathDev, envConfigFileDev); 
 
-console.log(`environment.ts generados con apiUrl=${backendUrl}`);
+console.log(`✅ environment.prod.ts y environment.ts generados con apiUrl=${backendUrl}`);
