7e8d2cfbea5ebbe41c757228d0be09945a451b3c
PieroSanchez <pierosancas@gmail.com>
2026-04-16 12:28:00 -0500

    cambios front

--- src/app/components/case-studies-projects/case-studies-projects.component.ts
+++ src/app/components/case-studies-projects/case-studies-projects.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-case-studies-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './case-studies-projects.component.html',
   styleUrl: './case-studies-projects.component.css'
 })
